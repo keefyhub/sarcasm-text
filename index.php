5888280dc0096e43d61b3762e1cd3f113c049737
Keith Light <keith.light@strawberrysoup.co.uk>
2019-11-07 11:20:23 +0000

    Add var_dump

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ if (getenv('TOKEN')) {
     define('TOKEN', $_ENV['TOKEN']);
 }
 
+var_dump(TOKEN);
+
 # Grab some of the values from the slash command, create vars for post back to Slack
 $command = !empty($_REQUEST['command']) ? $_REQUEST['command'] : false;
 $text = !empty($_REQUEST['text']) ? htmlspecialchars($_REQUEST['text']) : false;
